e16f3cfb1f3834b42d92fc463bf616e37528c2f2
Abdullah Qadri <aq34@student.le.ac.uk>
2024-02-22 02:51:52 +0000

    fixed issues

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,15 +2,21 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
- 
+import {
+    fetchRevenue,
+    fetchLatestInvoices,
+    fetchCardData,
+  } from '@/app/lib/data';
+   
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
-    const totalPaidInvoices = await fetchCardData().totalPaidInvoices;
-    const totalPendingInvoices = await fetchCardData().totalPendingInvoices;
-    const numberOfInvoices = await fetchCardData().numberOfInvoices;
-    const numberOfCustomers = await fetchCardData().numberOfCustomers;
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+      } = await fetchCardData();
 
   return (
     <main>
